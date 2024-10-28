a8021d31a1189145f4479bfafba8bc207161bfa8
Stormeta <103525509+Stormeta@users.noreply.github.com>
2024-10-28 16:53:55 +0000

    reverseString removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const arrayOfstring = string.split('');
+    const arrayReversed = arrayOfstring.reverse();
+    const reversedString = arrayReversed.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...removeThis) {
+    // Criar uma cópia do array original para não modificá-lo diretamente
+    const novoArray = [...array];
+
+    // Percorrer os elementos a remover
+    for (let i = 0; i < removeThis.length; i++) {
+        let indice = novoArray.indexOf(removeThis[i]);
+        // Se o elemento existir no array (índice diferente de -1)
+        while (indice !== -1) {
+            // Remover o elemento do array
+            novoArray.splice(indice, 1);
+            // Procurar novamente o elemento no array, a partir do índice anterior
+            indice = novoArray.indexOf(removeThis[i], indice);
+        }
+    }
+    return novoArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
