73b264ec469271f2ba7f0597112e73c941262d73
JoaoRodrigues <vclstorm@gmail.com>
2024-12-13 19:21:22 +0000

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
 
+    let finalSum = 0;
+
+    if (
+        typeof num1 !== 'number' ||
+        typeof num2 !== 'number' ||
+        num1 < 0 ||
+        num2 < 0 ||
+        !Number.isInteger(num1) ||
+        !Number.isInteger(num2)
+    ) {
+
+        return 'ERROR'
+    }
+    else if (num1 < num2) {
+        for (let contador = num1; contador <= num2; contador++) {
+            finalSum += contador;
+        }
+    } else {
+        for (let contador = num2; contador <= num1; contador++) {
+            finalSum += contador;
+        }
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
